63c992fcff5931eb21ce48cdeecd672714a48f65
Raccoon <a60814billy@gmail.com>
2015-06-23 12:54:09 +0800

    final

--- db/migrate/20150622211849_create_exhibits.rb
+++ db/migrate/20150622211849_create_exhibits.rb
@@ -2,7 +2,7 @@ class CreateExhibits < ActiveRecord::Migration
   def change
     create_table :exhibits do |t|
       t.references :activity
-      t.string :nae
+      t.string :name
       t.string :pic
       t.string :src
       t.timestamps null: false
